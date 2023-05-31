543a6b3a96febc5358c6e7e6eb77178164441984
JuanPablo <juan.roblesa@usantoto.edu.co>
2023-05-30 22:12:27 -0500

    VAR BASE DE DATOS

--- src/config/domains/var_database.js
+++ src/config/domains/var_database.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     user: "autobochica",
-    host: "autobochica.crilvnu5gqwb.us-east-1.rds.amazonaws.com",
-    database: "autobochica_db",
-    password: "bichis162332",
+    host: "autobochica.cwikakhedlsx.us-east-1.rds.amazonaws.com",
+    database: "autobochica",
+    password: "12345678",
     port: 5432,
 };
